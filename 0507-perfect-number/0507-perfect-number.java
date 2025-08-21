6d0cd62fa5ff76268fa7f7c22912077636318d05
Gurleenkour25 <166947092+Gurleenkour25@users.noreply.github.com>
2025-08-21 11:55:58 +0530

    Time: 2 ms (79.68%), Space: 40.4 MB (73.54%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -3,17 +3,17 @@ class Solution {
         if(n==1){
             return false;
         }
-        int sum =1;
-        for(int i =2;i<=(int)Math.sqrt(n);i++)
-        {
-            if(n%i ==0)
-            {
-                sum += i+(n/i);
+        int sum =1 ;
+        for(int i = 2 ;i<=(int)Math.sqrt(n) ; i++) {
+            if(n%i == 0) {
+                sum = sum + i + (n/i);
             }
         }
-        if(sum == n){
+        
+        if(sum == n) {
             return true;
         }
+
         return false;
     }
 }
