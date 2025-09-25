4a749c5a852bc4450c749113636af21b5134bc5c
Gurleenkour25 <166947092+Gurleenkour25@users.noreply.github.com>
2025-09-25 17:05:14 +0530

    Time: 2 ms (100%), Space: 44.8 MB (29.13%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,59 +1,14 @@
 class Solution {
+        static final int[] values =    {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    static final String[] symbols ={"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     public String intToRoman(int num) {
-        StringBuilder sb = new StringBuilder();
-        converter(sb,num/1000,'M','*','*');
-        num = num%1000;
-
-        converter(sb,num/100,'C','D','M');
-        num = num % 100;
-
-        converter(sb,num/10,'X','L','C');
-        num = num % 10;
-        
-        converter(sb,num,'I','V','X');
-        
-        return sb.toString();
+    StringBuilder roman = new StringBuilder();
+    for(int i=0;i<values.length && num > 0;i++){
+          while(num>=values[i]){
+           roman.append(symbols[i]);
+           num-=values[i];
+        }
     }
-
-    public void converter(StringBuilder sb,int val , char minor, char mid ,char major)
-    {
-        if(val >=1 && val <= 3)
-        {
-            for(int i =1 ;i<=val ; i++)
-            {
-                sb.append(minor);
-            }
-        }
-        else if (val == 4)
-        {
-            sb.append(minor);
-            sb.append(mid);
-        }
-
-        else if (val == 5)
-        {
-        
-            sb.append(mid);
-        }
-
-        else if (val >=6 && val <=8)
-        {
-            
-            sb.append(mid);
-            for(int i =6 ;i<=val ;i++)
-            {
-                sb.append(minor);
-            }
-        }
-
-
-      else if (val == 9)
-        {
-        
-            sb.append(minor);
-            sb.append(major);
-        }
-
-
+    return roman.toString();
     }
-}
+    }
