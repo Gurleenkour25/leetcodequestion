f7fa20a3bcd5abba4cf09077a9be1237f8597b21
Gurleenkour25 <166947092+Gurleenkour25@users.noreply.github.com>
2025-10-09 22:40:06 +0530

    Time: 143 ms (27.02%), Space: 45.5 MB (20.17%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,32 +1,32 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        int maxlen =1;
+    public String longestPalindrome(String s) 
+    { 
+        int maxlen = 1;
         int start = 0;
-
-        for(int i = 0 ;i<s.length() ; i++)
+        for(int i =0 ;i < s.length() ; i++)
         {
-            int length1 = expand(s,i,i);
-            int length2 = expand(s,i,i+1);
-            int len = Math.max(length1,length2);
+            int len1 = expand(s,i,i);
+            int len2 = expand(s,i,i+1);
+            int len = Math.max(len1,len2);
 
-            if(len>maxlen)
+            if(len > maxlen)
             {
                 maxlen = len;
-                start = i-(len-1)/2;
+                start = i - (len-1)/2;
             }
         }
-        return s.substring(start,start+maxlen);
+        return s.substring(start , start+maxlen);
     }
 
-    public int expand(String s , int left , int right)
+    public int expand(String s ,int left ,int right)
     {
         char[] arr = s.toCharArray();
-        while(left>=0 && right<=arr.length-1 && arr[left]==arr[right])
+        while(left >= 0 && right <= arr.length -1 && arr[left] == arr[right])
         {
             left--;
             right++;
         }
 
-        return (right-left)-1;
+        return(right - left)-1;
     }
 }
