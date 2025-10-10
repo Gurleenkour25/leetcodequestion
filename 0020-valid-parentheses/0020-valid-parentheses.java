8df4379cc97a643ea304f9926c4d6132bcbe4f2c
Gurleenkour25 <166947092+Gurleenkour25@users.noreply.github.com>
2025-10-10 14:01:03 +0530

    Time: 3 ms (54.47%), Space: 42.2 MB (22.45%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,37 +1,32 @@
 class Solution {
     public boolean isValid(String s) {
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-        char[] arr = s.toCharArray();
+       char[] arr = s.toCharArray(); 
 
-        for(int i = 0 ; i < arr.length; i++)
+       ArrayDeque<Character> stack = new ArrayDeque<>();
+       for(int i =0 ;i<arr.length ; i++)
+       {
+       if(arr[i]=='(' ||arr[i]=='[' || arr[i]=='{')
+       {
+            stack.push(arr[i]);
+       } 
+        else if(arr[i] == ')' || arr[i] == '}' ||arr[i] == ']')
         {
-            if(arr[i]=='(' || arr[i]=='{' || arr[i]=='[')
-            {
-                stack.push(arr[i]);
-            }
-            else if(arr[i]==')' || arr[i]=='}' || arr[i]==']')
-            {
-                if(stack.isEmpty()) return false;
+            if(stack.isEmpty()) return false;
+            char top = stack.pop();
 
-               char top = stack.pop();
-               
-                if(arr[i]==')' && top != '(' || arr[i]=='}' && top != '{' ||arr[i]==']' && top != '[')
-                {
-                    return false;
-                }
+            if(arr[i]=='}' && top != '{' || arr[i]==')' && top != '(' || arr[i]==']' && top != '[')
+            {
+                   return false;
             }
 
         }
+       }
+       
+       if(stack.isEmpty())
+       {
+        return true;
+       }
 
-               if(stack.isEmpty())
-               {
-                return true;
-               }
-
-              return false;
-            }
-        }
-
-
-        
-    
+       return false;
+    }
+}
