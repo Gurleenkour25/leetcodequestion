6d96f723f78facaa79175e3a01b5ea3b83f59dda
Gurleenkour25 <166947092+Gurleenkour25@users.noreply.github.com>
2025-10-10 17:38:39 +0530

    Time: 22 ms (99.15%), Space: 55.9 MB (6.36%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,24 +1,26 @@
-import java.util.Stack;
-
 class StockSpanner {
-    // Stack stores pairs: [price, span]
-    private Stack<int[]> stack;
-
+private ArrayDeque<int[]> stack;
     public StockSpanner() {
-        stack = new Stack<>();
+      stack = new ArrayDeque<>();  
     }
-
+    
     public int next(int price) {
-        int span = 1;
-
-        // Merge spans of previous prices <= current
-        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+       int span =1;
+        
+        while(!stack.isEmpty() && stack.peek()[0] <=price )
+        {  
+       
             span += stack.pop()[1];
         }
 
-        // Push current price and its span
-        stack.push(new int[]{price, span});
+        stack.push(new int[]{price,span});
 
         return span;
     }
 }
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
