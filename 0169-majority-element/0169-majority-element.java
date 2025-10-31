38388b5152f063cf43fdf829ec36bd9e5e48c4c8
Gurleenkour25 <166947092+Gurleenkour25@users.noreply.github.com>
2025-10-31 19:30:52 +0530

    Time: 2 ms (75.3%), Space: 55.6 MB (8.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-      int cand = nums[0];
-      int vote = 0 ;
+        int cand = 0;
+        int vote = 0;
 
-      for(int i =0 ; i<nums.length ; i++)
-      {  
-        if(vote == 0) {
-            cand = nums[i];
-        }
-       else if(nums[i] == cand){
-          vote++;
-        }
-        else {
-            vote--;
-        }
-         
-      }  
+        for(int i = 0 ; i< nums.length ; i++) {
+            if(vote == 0)
+            {
+                cand = nums[i];
+            }
 
-      return cand;
+            if(nums[i] == cand) {
+                vote++;
+            }
+            else{
+                vote--;
+            }
+        }
+
+        return cand;
     }
 }
