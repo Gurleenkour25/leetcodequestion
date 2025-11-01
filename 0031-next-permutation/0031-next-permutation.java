f882c66cf86554c53d6e0f78a2eb962c9db2e166
Gurleenkour25 <166947092+Gurleenkour25@users.noreply.github.com>
2025-11-01 22:51:01 +0530

    Time: 0 ms (100%), Space: 43.2 MB (57.81%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -6,7 +6,7 @@ class Solution {
          // find the pivot ---- always the number decreasing from the next number to it
         for(int i = n-2 ;i>=0 ;i--)
         {
-            if(nums[i]<nums[i+1])
+            if(nums[i] < nums[i+1])
             {
                 pivot = i;
                 break;
@@ -14,31 +14,36 @@ class Solution {
         }
 
       // if no largest no acn be formed after than just sort the number and return
-        if(pivot == -1)
-        {
-            Arrays.sort(nums);
-            return;
-        }
 
+      if(pivot == -1)
+      {
+         reverse(nums,0,n-1);
+         return;
+      }
+        
         //step 2 :- if we any number greate than after the pivot we will swap them with the pivot
-
-        for(int i= n-1 ;i>=pivot ;i--)
+   
+      for(int i = n-1 ; i >= pivot ; i--)
+      {
+        if(nums[i] > nums[pivot])
         {
-            if(nums[i]>nums[pivot])
-            {
-                int temp = nums[pivot];
-                nums[pivot] = nums[i];
-                nums[i] = temp;
-                break;
-            }
-        }
+            int temp = nums[i];
+            nums[i] = nums[pivot];
+            nums[pivot] = temp;
 
+            break;
+        }
+      }
+        
         // step 3:- we will reverse after the pivot
+
         reverse(nums,pivot+1,n-1);
+       
     }
 
-    public void reverse(int[] arr, int start , int end) {
-        while (start<end)
+    public void reverse(int[] arr , int start , int end)
+    {
+        while(start < end)
         {
             int temp = arr[start];
             arr[start] = arr[end];
@@ -47,4 +52,7 @@ class Solution {
             end--;
         }
     }
+
+    
+    
 }
