0edf9c15584f6ca0122ce7fbf5618baadddba45c
Gurleenkour25 <166947092+Gurleenkour25@users.noreply.github.com>
2025-11-01 23:11:46 +0530

    Time: 0 ms (100%), Space: 44.5 MB (5.83%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -3,8 +3,7 @@ class Solution {
         int n = nums.length;
         int pivot = -1;
 
-         // find the pivot ---- always the number decreasing from the next number to it
-        for(int i = n-2 ;i>=0 ;i--)
+        for(int i = n-2 ; i >=0 ; i--)
         {
             if(nums[i] < nums[i+1])
             {
@@ -13,46 +12,38 @@ class Solution {
             }
         }
 
-      // if no largest no acn be formed after than just sort the number and return
-
-      if(pivot == -1)
-      {
-         reverse(nums,0,n-1);
-         return;
-      }
-        
-        //step 2 :- if we any number greate than after the pivot we will swap them with the pivot
-   
-      for(int i = n-1 ; i >= pivot ; i--)
-      {
-        if(nums[i] > nums[pivot])
+        if(pivot == -1)
         {
-            int temp = nums[i];
-            nums[i] = nums[pivot];
-            nums[pivot] = temp;
+            reverse(nums,0,n-1);
 
-            break;
+            return;
         }
-      }
-        
-        // step 3:- we will reverse after the pivot
 
-        reverse(nums,pivot+1,n-1);
-       
+        for(int i = n-1 ; i > pivot ;i--)
+        {
+            if(nums[i] > nums[pivot])
+            {
+                int temp = nums[i];
+                nums[i] = nums[pivot];
+                nums[pivot] = temp;
+
+                break;
+            }
+         }
+
+         reverse(nums,pivot+1,n-1);
     }
 
-    public void reverse(int[] arr , int start , int end)
+    public void reverse(int[]arr , int start , int end)
     {
         while(start < end)
         {
             int temp = arr[start];
             arr[start] = arr[end];
             arr[end] = temp;
+
             start++;
             end--;
         }
     }
-
-    
-    
 }
