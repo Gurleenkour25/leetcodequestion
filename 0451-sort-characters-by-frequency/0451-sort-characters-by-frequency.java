f3f957fe5f41d28be349fa22fabdbe464d2ea9cf
Gurleenkour25 <166947092+Gurleenkour25@users.noreply.github.com>
2025-11-24 15:35:32 +0530

    Time: 5 ms (96.61%), Space: 46.3 MB (33.78%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,35 +1,36 @@
 class Solution {
     public String frequencySort(String s) {
+      int[] freq = new int[256];
 
-        // Step 1: count frequency
-        int[] freq = new int[256];
-        for (char ch : s.toCharArray()) {
-            freq[ch]++;
+      for(char ch : s.toCharArray())
+      {
+          freq[ch]++;
+      }  
+
+     PriorityQueue<Character> pq = new PriorityQueue<>( (a, b) -> freq[b] - freq[a]);
+
+       
+       for(int i = 0 ; i < 256 ; i++)
+       {
+           if(freq[i] > 0)
+           {
+             pq.add((char)i);
+           }
+       }
+
+       StringBuilder str = new StringBuilder();
+
+      while(!pq.isEmpty()) {
+        char ch = pq.poll();
+        int count = freq[ch];
+
+        for(int i = 0 ; i < count ; i++) 
+        {
+            str.append(ch);
         }
+      }
 
-        // Step 2: build max heap
-        PriorityQueue<Character> pq = new PriorityQueue<>(
-            (a, b) -> freq[b] - freq[a]   // max-heap based on frequency
-        );
 
-        // Step 3: add all characters with freq > 0
-        for (int i = 0; i < 256; i++) {
-            if (freq[i] > 0) {
-                pq.add((char) i);
-            }
-        }
-
-        // Step 4: build result
-        StringBuilder sb = new StringBuilder();
-        while (!pq.isEmpty()) {
-            char ch = pq.poll();       // highest freq char
-            int count = freq[ch];
-
-            for (int i = 0; i < count; i++) {
-                sb.append(ch);
-            }
-        }
-
-        return sb.toString();
+        return str.toString();
     }
 }
