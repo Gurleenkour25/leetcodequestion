152fbb4e2def739009dac7f90b536d432213f6b6
Gurleenkour25 <166947092+Gurleenkour25@users.noreply.github.com>
2025-11-25 15:25:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,34 +1,21 @@
 class Solution {
     public int romanToInt(String s) {
-        int pre = 0;
-        int num =0;
         int n = s.length();
-        for(int i = n-1 ; i >= 0 ; i--) {
-           
-             int value = switch(s.charAt(i))
-             {
-                case 'I'-> 1;
-                case 'V'-> 5;
-                case 'X'-> 10;
-                case 'L'-> 50;
-                case 'C'->100;
-                case 'D'-> 500;
-                case 'M'-> 1000;
-                default -> 0;
-             };
+        int num =0;
+        int pre = 0;
+        for(int i = n-1 ; i >= 0 ; i-- )
+        {
+            int value = switch(s.charAt(i))
+            {case 'I' ->1;
+            case 'I' ->1;
+            case 'I' ->1;
+            case 'I' ->1;
+            
 
-             if(value < pre)
-             {
-               num = num - value;
-             }
+            
 
-             else {
-                num = num + value;
-             }
 
-             pre = value;
+            };
         }
-
-        return num;
     }
 }
