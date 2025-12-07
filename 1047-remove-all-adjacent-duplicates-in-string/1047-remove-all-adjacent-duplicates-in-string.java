63198305f4f0ed4e5ef9e1d3d4de2867737f6897
Gurleenkour25 <166947092+Gurleenkour25@users.noreply.github.com>
2025-12-07 17:19:03 +0530

    Time: 19 ms (61.5%), Space: 47.1 MB (28.73%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,25 +1,32 @@
 class Solution {
     public String removeDuplicates(String s) {
-       ArrayDeque<Character> stack = new ArrayDeque<>();
-       char[] arr = s.toCharArray();
-
-       for(int i = 0 ;i<arr.length ; i++)
-       {
-          if(!stack.isEmpty() && stack.peek()==arr[i])
-          {
-            stack.pop();
-          }
-          else{
+      Deque<Character> stack = new ArrayDeque<>();
+      char[] arr = s.toCharArray();
+      for(int i =0 ; i < arr.length ; i++)
+      {
+        if(!stack.isEmpty() && stack.peek() == arr[i])
+        {
+           stack.pop();
+        }
+        else{
             stack.push(arr[i]);
-          }
-       }
-    
-       StringBuilder ss = new StringBuilder();
-       for(char c : stack)
-       {
-          ss.append(stack.pop());
-       }
-          
-      return ss.reverse().toString();
+        }
+      }
+
+      if(stack.isEmpty())
+      {
+        return "";
+      }
+
+      StringBuilder str = new StringBuilder();
+
+      while(!stack.isEmpty())
+      {
+        str.append(stack.pop());
+      }
+      str.reverse();
+
+      return str.toString();
+
     }
 }
