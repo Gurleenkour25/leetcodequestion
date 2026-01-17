431f46754d560ced71de3f1a0b3f18d430cb5826
Gurleenkour25 <166947092+Gurleenkour25@users.noreply.github.com>
2026-01-17 12:28:10 +0530

    Time: 0 ms (100%), Space: 43.1 MB (43.09%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,40 +1,66 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        StringBuilder str = new StringBuilder();
-        StringBuilder str1 = new StringBuilder();
+        
+        int i = s.length()-1;
+        int j = t.length()-1;
 
-        for(char c: s.toCharArray())
+        int ss = 0;
+        int st = 0;
+
+        while(i >= 0 || j >= 0)
         {
-            if(c != '#')
+            while(i >=0)
             {
-                str.append(c);
+                if(s.charAt(i) == '#')
+                {
+                    ss++;
+                    i--;
+                }
+                else if(ss > 0)
+                {
+                    ss--;
+                    i--;
+                }
+                else {
+                    break;
+                }
             }
-            else if(c== '#')
+
+
+            while( j >= 0){
+                if(t.charAt(j) == '#'){
+                    st++;
+                    j--;
+                }
+                else if(st > 0){
+                    st--;
+                    j--;
+                }
+                else{
+                    break;
+                }
+            }
+
+            if(i >= 0 && j >= 0){
+                if(s.charAt(i) != t.charAt(j))
+                {
+                    return false;
+                }
+
+            }
+
+            else
+            { 
+                if(i >=0 || j >=0)
             {
-                if(str.length() > 0)
-                str.deleteCharAt(str.length() -1);
+                return false;
             }
+            }
+
+            i--;
+            j--;
         }
 
-
-        for(char c: t.toCharArray())
-        {
-            if(c != '#')
-            {
-                str1.append(c);
-            }
-            else if(c=='#')
-            {
-                  if(str1.length() > 0)
-                str1.deleteCharAt(str1.length() -1);
-            }
-        }
-
-        if(str.toString().equals(str1.toString()))
-        {
-            return true;
-        }
-
-        return false;
+        return true;
     }
 }
